4281fd8749b066baa2d43ca169cccc61c553d0c2
JavierCastill04 <indiana1380@gmail.com>
2026-09-04 11:37:44 -0600

    Agregar datos iniciales para prueba

---
+++ src/data/funcionesData.ts
@@ -0,0 +1,8 @@
+import { Funcion } from "../types/Funcion";
+
+export const funcionesData: Funcion[] = [
+    { id: 1, salaId: 1, peliculaId: 1, fecha: "2026-07-22", horaInicio: "18:30", precio: 6.5, estadoAsientos: [] },
+    { id: 2, salaId: 2, peliculaId: 2, fecha: "2026-07-21", horaInicio: "14:00", precio: 5.5, estadoAsientos: [] },
+    { id: 3, salaId: 3, peliculaId: 3, fecha: "2026-07-25", horaInicio: "09:20", precio: 6, estadoAsientos: [] },
+    { id: 4, salaId: 4, peliculaId: 4, fecha: "2026-07-28", horaInicio: "21:40", precio: 7.5, estadoAsientos: [] },
+];
---
+++ src/data/salasData.ts
@@ -0,0 +1,8 @@
+import {Sala} from "../types/Sala";
+
+export const salasData: Sala[] = [
+    {id: 1, nombre: "Sala 1", capacidad: {filas: 12, columnas: 15}, asientos: []},
+    {id: 2, nombre: "Sala 2", capacidad: {filas: 12, columnas: 15}, asientos: []},
+    {id: 3, nombre: "Sala 3", capacidad: {filas: 12, columnas: 15}, asientos: []},
+    {id: 4, nombre: "Sala 4", capacidad: {filas: 12, columnas: 15}, asientos: []}
+];
