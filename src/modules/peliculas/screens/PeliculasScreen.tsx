c36828c4b97125ea0e28c9480a473cee24dd0640
JavierCastill04 <indiana1380@gmail.com>
2026-09-04 12:09:01 -0600

    Ejemplo de utilización de redux

--- src/modules/peliculas/screens/PeliculasScreen.tsx
+++ src/modules/peliculas/screens/PeliculasScreen.tsx
@@ -1,13 +1,35 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text, View } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+
+import { useAppSelector, useAppDispatch } from '../../../redux/hooks';
+import { removePelicula } from '../../../redux/slices/peliculaSlice';
 
 export default function PeliculasScreen() {
-    return (
-        <SafeAreaView>
-            <View>
-                <Text>Estás en Peliculas</Text>
-            </View>
-        </SafeAreaView>
 
+    const peliculas = useAppSelector(
+        (state) => state.pelicula
+    );
+
+    const dispatch = useAppDispatch();
+
+    const borrarPelicula = (id: number) => {
+        dispatch(removePelicula(id));
+    }
+
+    return (
+        <View>
+            <Text>Películas: {peliculas.length}</Text>
+            <FlatList
+                data={peliculas}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>{item.nombre}</Text>
+                        <TouchableOpacity onPress={() => borrarPelicula(item.id)}>
+                            <Text>Borrar</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+        </View>
     );
 }
